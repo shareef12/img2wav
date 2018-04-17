d00924f0da53fca5f37db64f4ad42c2b06476522
Christian Sharpsten <chris.sharpsten@gmail.com>
2018-04-16 23:11:14 -0400

    Bump version number for pypi

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "img2wav",
-    version = "0.1.0",
+    version = "0.1.1",
     description = "Convert images to wav audio files to view in a spectrogram",
     author = "Christian Sharpsten",
     author_email = "chris.sharpsten@gmail.com",
     license = "GPL-2",
     url = "https://github.com/shareef12/img2wav",
-    download_url = "https://github.com/shareef12/img2wav/archive/v0.1.0.tar.gz",
+    download_url = "https://github.com/shareef12/img2wav/archive/v0.1.1.tar.gz",
     py_modules = ["img2wav"],
     install_requires = [
         "numpy",
