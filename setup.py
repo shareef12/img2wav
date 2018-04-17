84fe6c4376e9532d93c03b14dff522bec0cfdfdd
Christian Sharpsten <chris.sharpsten@gmail.com>
2018-04-16 23:04:30 -0400

    Update setup.py in preparation for pypi upload

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@
 
 from setuptools import setup, find_packages
 
-setup(name="img2wav",
-      version="0.1.0",
-      description="Convert images to wav audio files to view in a spectrogram",
-      author="Christian Sharpsten",
-      author_email="chris.sharpsten@gmail.com",
-      packages=find_packages(),
-      install_requires=[
-          "numpy",
-          "Pillow",
-      ],
-      entry_points={
-          "console_scripts": [
-              "img2wav = img2wav:main",
-          ],
-      })
+setup(
+    name = "img2wav",
+    version = "0.1.0",
+    description = "Convert images to wav audio files to view in a spectrogram",
+    author = "Christian Sharpsten",
+    author_email = "chris.sharpsten@gmail.com",
+    license = "GPL-2",
+    url = "https://github.com/shareef12/img2wav",
+    download_url = "https://github.com/shareef12/img2wav/archive/v0.1.0.tar.gz",
+    py_modules = ["img2wav"],
+    install_requires = [
+        "numpy",
+        "Pillow",
+    ],
+    entry_points = {
+        "console_scripts": [
+            "img2wav = img2wav:main",
+        ],
+    })
